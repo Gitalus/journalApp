21abd733a2de18c24f7c6250bcc4ec75216f6517
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-20 12:24:50 -0300

    add jsx to content and style

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,22 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
             <NotesAppBar />
+
+            <div className="notes__content">
+                <input
+                    className="notes_title-input"
+                    type="text"
+                    placeholder="Some awesome title"
+                    autoComplete="off"
+                />
+                <textarea
+                    placeholder="What happened today"
+                    className="notes_textarea">
+                </textarea>
+                <div className="notes__images">
+                    <img src="https://www.iekakmi-roadtrips.gr/plugins/fullpage/examples/imgs/bg2.jpg" alt="imagen" />
+                </div>
+            </div>
         </div>
     )
 }
