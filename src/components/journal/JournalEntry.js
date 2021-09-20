8c2a24bf0cc2511e0328f6d6562abae9a41b69c4
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-19 22:16:03 -0300

    complete schema for sidebar and style it

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const JournalEntry = () => {
+    return (
+        <div className="journal__entry">
+            <div
+                className="journal__entry-picture"
+                style={{
+                    backgroundSize: "cover",
+                    backgroundImage: "url(https://www.iekakmi-roadtrips.gr/plugins/fullpage/examples/imgs/bg2.jpg)"
+                }}
+            ></div>
+            <div className="journal__entry-body">
+                <p className="journal__entry-title">
+                    Un nuevo día
+                </p>
+                <p className="journal__entry-content">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia optio sed nulla dignissimos nisi iure.
+                </p>
+            </div>
+            <div className="journal__entry-date-box">
+                <span>Monday</span>
+                <h4>28</h4>
+            </div>
+        </div>
+    )
+}
