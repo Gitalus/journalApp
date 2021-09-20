02a26ff5190e766a4f6a351c5ab1cba2eed6e79b
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-20 09:21:19 -0300

    create addBar and style it

---
+++ src/components/notes/NotesAppBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const NotesAppBar = () => {
+    return (
+        <div className="notes__appbar">
+            <span>28 de agosto 2020</span>
+            <button className="btn">
+                Picture
+            </button>
+
+            <button className="btn">
+                Save
+            </button>
+        </div>
+    )
+}
