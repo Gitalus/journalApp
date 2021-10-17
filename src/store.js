779cbe4dc0db97aabc3eac6c9a54574055656a73
Italo Guajardo <italog.penaloza@gmail.com>
2021-10-17 20:31:34 -0300

    add middleware in reducer login

--- src/store.js
+++ src/store.js
@@ -1,11 +1,19 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { authReducer } from './reducers/authReducer';
+
+// añade más de un middleWare
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const reducers = combineReducers({
     auth: authReducer
 });
 
+
+// el middleware thunk se disparará cuando se encuentre una acción async
 export const store = createStore(
     reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
 );
